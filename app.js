cdf88e4f1bfbadc8bee7f69205a94f68a63ccd42
mohamed ouanjine <email.for.tv.elhajji@gmail.com>
2023-11-12 17:22:27 +0100

    somme stuff

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const { pool } = require('./db/index.js');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
