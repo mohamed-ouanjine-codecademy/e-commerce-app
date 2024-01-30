69aa3fde9f41592d1a67cd5034a148251e6a004c
mohamed-ouanjine-codecademy <email.for.tv.elhajji@gmail.com>
2024-01-30 19:46:46 +0100

    fix the typo and add active class when link is active

--- view/src/components/Footer.js
+++ view/src/components/Footer.js
@@ -8,26 +8,26 @@ export function Footer() {
         <div className='row gap-5'>
           <div className='col'>
             <h3>Links</h3>
-            <ul className="nav flex-column">
+            <ul className="nav nav-underline flex-column">
               <li className='nav-item'>
-                <Link className='nav-link' to="/">Home</Link>
+                <Link className={`nav-link ${({ isActive }) => isActive && 'active'}`} to="/">Home</Link>
               </li>
               <li className='nav-item'>
-                <Link className='nav-link' to="#">Terms</Link>
+                <Link className={`nav-link ${({ isActive }) => isActive && 'active'}`} to="#">Terms</Link>
               </li>
               <li className='nav-item'>
-                <Link className='nav-link' to="#">Privacy</Link>
+                <Link className={`nav-link ${({ isActive }) => isActive && 'active'}`} to="#">Privacy</Link>
               </li>
             </ul>
           </div>
           <div className='col'>
             <h3>Auth</h3>
-            <ul className="nav flex-column">
+            <ul className="nav nav-underline flex-column">
               <li className='nav-item'>
-                <Link className='nav-link' to="/sign-up">Sign UP</Link>
+                <Link className={`nav-link ${({ isActive }) => isActive && 'active'}`} to="/sign-up">Sign Up</Link>
               </li>
               <li className='nav-item'>
-                <Link className='nav-link' to="/sign-in">Sign In</Link>
+                <Link className={`nav-link ${({ isActive }) => isActive && 'active'}`} to="/sign-in">Sign In</Link>
               </li>
             </ul>
           </div>
