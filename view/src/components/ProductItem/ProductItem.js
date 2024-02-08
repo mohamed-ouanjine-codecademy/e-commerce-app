a93951d03a44b47598d6ae8edf4f80c898938af4
mohamed-ouanjine-codecademy <email.for.tv.elhajji@gmail.com>
2024-02-08 11:16:46 +0100

    fix product item leyout

--- view/src/components/ProductItem/ProductItem.js
+++ view/src/components/ProductItem/ProductItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './ProductItem.module.css';
 import { AddToCartButton } from '../AddToCartButton';
 
 export function ProductItem({ className, product }) {
   return (
     <>
-      <div className={`${className} card`} >
-          <img src={product.imageUrl} className="card-img-top" alt={product.name} />
+      <div className={`${className} card ${styles.itemContainer}`} >
+        <div className={`${styles.imageContainer}`} style={{ backgroundImage: `url(${product.imageUrl})`}}>
+          <img src={product.imageUrl} className={`${styles.image} card-img-top`} alt={product.name} />
+        </div>
         <div className="card-body">
           <h5 className="card-title">{product.name}</h5>
           <h6 className="card-subtitle mb-2 text-body-secondary">{product.price}</h6>
